a34f02bc56087e5ab20b2088b7755dbbbf45ea4b
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2017-12-18 16:15:48 -0200

    Aula3
    Conteúdo desenvolvido até o fim da aula 3

---
+++ Aula3/ByteBank.Portal/ByteBank.Portal/Infraestrutura/IoC/ContainerSimples.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal.Infraestrutura.IoC
+{
+    public class ContainerSimples : IContainer
+    {
+        private readonly Dictionary<Type, Type> _mapaDeTipos = new Dictionary<Type, Type>();
+
+        // Registrar( typeof(ICambioService), typeof(CambioServiceTeste) );
+        
+            
+            // Registrar( typeof(ICambioService), typeof(CartaServiceTeste) );o
+        
+            
+            // Recuperar( typeof(ICambioService) )
+        //   deve retornar para nós uma instância do tipo CambioServiceTeste
+        public void Registrar(Type tipoOrigem, Type tipoDestino)
+        {
+            if (_mapaDeTipos.ContainsKey(tipoOrigem))
+                throw new InvalidOperationException("Tipo já mapeado!");
+
+            VerificarHierarquiaOuLancarExcecao(tipoOrigem, tipoDestino);
+
+            _mapaDeTipos.Add(tipoOrigem, tipoDestino);
+        }
+
+        public void Registrar<TOrigem, TDestino>() where TDestino : TOrigem
+        {
+            if (_mapaDeTipos.ContainsKey(typeof(TOrigem)))
+                throw new InvalidOperationException("Tipo já mapeado!");
+
+            _mapaDeTipos.Add(typeof(TOrigem), typeof(TDestino));
+        }
+
+        private void VerificarHierarquiaOuLancarExcecao(Type tipoOrigem, Type tipoDestino)
+        {
+            // Verificar se tipoDestino herda ou implementa tipoOrigem
+            if(tipoOrigem.IsInterface)
+            {
+                var tipoDestinoImplementaInterface = 
+                    tipoDestino
+                        .GetInterfaces()
+                        .Any(tipoInterface => tipoInterface == tipoOrigem);
+
+                if (!tipoDestinoImplementaInterface)
+                    throw new InvalidOperationException("O tipo destino não implementa a interface");
+            }
+            else
+            {
+                var tipoDestinoHerdaTipoOrigem = tipoDestino.IsSubclassOf(tipoOrigem);
+                if (!tipoDestinoHerdaTipoOrigem)
+                    throw new InvalidOperationException("O tipo destino não herda o tipo de origem");
+            }
+        }
+
+        public object Recuperar(Type tipoOrigem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
