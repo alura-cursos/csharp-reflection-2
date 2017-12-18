a021a9d11f3d5a160dc9f9abcc43ce7815925a40
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2017-12-18 16:11:09 -0200

    Aula1
    Conteúdo desenvolvido até o fim da aula 1

---
+++ Aula1/ByteBank.Portal/ByteBank.Portal/Controller/CambioController.cs
@@ -0,0 +1,63 @@
+﻿using ByteBank.Portal.Infraestrutura;
+using ByteBank.Portal.Model;
+using ByteBank.Service;
+using ByteBank.Service.Cambio;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal.Controller
+{
+    public class CambioController : ControllerBase
+    {
+        private ICambioService _cambioService;
+
+        public CambioController()
+        {
+            _cambioService = new CambioTesteService();
+        }
+
+        public string MXN()
+        {
+            var valorFinal = _cambioService.Calcular("MXN", "BRL", 1);
+            var textoPagina = View();
+
+            var textoResultado = textoPagina.Replace("VALOR_EM_REAIS", valorFinal.ToString());
+
+            return textoResultado;
+        }
+
+        public string USD()
+        {
+            var valorFinal = _cambioService.Calcular("USD", "BRL", 1);
+            var textoPagina = View();
+
+            var textoResultado = textoPagina.Replace("VALOR_EM_REAIS", valorFinal.ToString());
+
+            return textoResultado;
+        }
+
+        public string Calculo(string moedaOrigem, string moedaDestino, decimal valor)
+        {
+            var valorFinal = _cambioService.Calcular(moedaOrigem, moedaDestino, valor);
+            var modelo = new CalculoCambioModel {
+                MoedaDestino = moedaDestino,
+                ValorDestino = valorFinal,
+                MoedaOrigem = moedaOrigem,
+                ValorOrigem = valor
+            };
+
+            return View(modelo);
+        }
+
+        public string Calculo(string moedaDestino, decimal valor) =>
+            Calculo("BRL", moedaDestino, valor);
+
+        public string Calculo(string moedaDestino) =>
+            Calculo("BRL", moedaDestino, 1);
+    }
+}
---
+++ Aula1/ByteBank.Portal/ByteBank.Portal/Model/CalculoCambioModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Portal.Model
+{
+    public class CalculoCambioModel
+    {
+        public string MoedaOrigem { get; set; }
+        public string MoedaDestino { get; set; }
+
+        public decimal ValorOrigem { get; set; }
+        public decimal ValorDestino { get; set; }
+    }
+}
